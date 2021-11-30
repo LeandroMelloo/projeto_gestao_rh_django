ee268fd689a6fba365c75201a1c3d561c9bacd20
leandropaulino <leandropaulino@brq.com>
2021-11-30 20:55:59 -0300

    criação app de empresa

---
+++ apps/empresas/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=100, help_text="Nome da empresa")
+
+    def __int__(self):
+        return self.id
+    
+    def __str__(self):
+        return self.nome
+
