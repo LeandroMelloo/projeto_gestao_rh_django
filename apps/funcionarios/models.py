e3e1c3c882c731eb08fb038da74b62ea2f0b5744
leandropaulino <leandropaulino@brq.com>
2021-12-01 19:30:59 -0300

    Implementando as relações dos modelos

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    usuario = models.OneToOneField(User, on_delete=models.PROTECT) # PROTECT -> primeiro deletar o funcionario, depois deletar usuario
+    departamento = models.ManyToManyField(Departamento) # ManyToManyField -> 1 Usuario <-> N Departamentos, um usuario irá possuir uma lista de departamentos
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT) # PROTECT -> primeiro deletar o funcionario, depois deletar empresa
 
     def __int__(self):
         return self.id
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT) # PROTECT -> primeiro deletar o Registro Hora Extra, depois deletar funcionario
 
     def __int__(self):
         return self.id
