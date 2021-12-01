146a4042c6bdb44a1b9adc7c176771d439423de2
leandropaulino <leandropaulino@brq.com>
2021-12-01 08:16:39 -0300

    Criando a app de funcionarios

---
+++ apps/funcionarios/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+
+
+    def __int__(self):
+        return self.id
+
+    def __str__(self):
+        return self.nome
