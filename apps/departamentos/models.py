c45a36a8716cffa04b30cfad1eeb2a438f915073
leandropaulino <leandropaulino@brq.com>
2021-12-01 08:23:32 -0300

    Criação da app de departamentos

---
+++ apps/departamentos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=70)
+
+
+    def __int__(self):
+        return self.id
+
+    def __str__(self):
+        return self.nome
